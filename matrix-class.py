b8c1fbe6851b05cbaeb4e8be63c59c0c6beb0ca8
janhuls <156079202+janhuls@users.noreply.github.com>
2024-01-11 15:08:47 +0100

    Update matrix-class.py

--- matrix-class.py
+++ matrix-class.py
@@ -1,4 +1,52 @@
-import numpy
-
+import numpy as np
 class matrix:
-    
+    def __init__(self,matrix) -> None:
+        
+        self.matrix = matrix
+        self.shape = matrix.shape
+
+    def __str__(self):
+        out = "[\n"
+        str_mat = np.char.mod("%d",self.matrix)
+        for i in range(self.shape[0]):
+            out+="["
+            for j in range(self.shape[1]):
+                out+=str_mat[i][j]+"    "
+            out = out[:-4]
+            out+="]\n"
+        out+="]"
+        return out
+
+    def __add__(self,other):
+        if self.shape!=other.shape:
+            raise ValueError("matrices hebben niet dezelfde vorm")
+            pass
+        solmat = matrix(np.zeros(self.shape))
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                solmat.matrix[i][j]= self.matrix[i][j]+other.matrix[i][j]
+        return solmat
+    def __radd__(self,other):
+        if self.shape!=other.shape:
+            raise ValueError("matrices hebben niet dezelfde vorm")
+            pass
+        solmat = matrix(np.zeros(self.shape))
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                solmat.matrix[i][j]= self.matrix[i][j]+other.matrix[i][j]
+        return solmat
+    def __mul__(self,other):
+        if self.shape[1]!=other.shape[0]:
+            raise ValueError("matrices kunnen niet vermenigvuldigd worden!")
+            pass
+        solmat = matrix(np.zeros((self.shape[0],other.shape[1])))
+        for i in range(self.shape[0]):
+            for j in range(other.shape[1]):
+                for k in range(other.shape[0]):
+                    solmat.matrix[i][j]+=self.matrix[i][k]*other.matrix[k][j]
+        return solmat
+y= np.array([[1,9,8],[2,9,8],[3,9,8]])
+A = matrix(y)
+x = np.array([[4,5,6],[7,8,9],[1,2,3]])
+B = matrix(x)
+print(A*B)
