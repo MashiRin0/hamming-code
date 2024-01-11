401d7ee891cd7f276871816fe040cbe231ef092b
MashiRin0 <s.garritsen@students.uu.nl>
2024-01-11 15:12:45 +0100

    Update matrix-class.py

--- matrix-class.py
+++ matrix-class.py
@@ -1,4 +1,23 @@
-import numpy
+import numpy as np
 
 class matrix:
     
+    def __init__(self, matrix) -> None:
+        
+        self.matrix = matrix
+        self.shape = matrix.shape()
+        
+
+    def RREF(self):
+
+        pass
+
+A = np.array([[3,1,2],[0,4,3],[1,7,2]])
+B = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+# A = np.zeros(shape=(3,2), dtype=int)
+
+# Poep = matrix(A)
+
+print(A)
+print(B)
