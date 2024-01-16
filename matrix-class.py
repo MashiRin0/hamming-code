d864b95a79985fe772421fd9b432ef8e41f0248f
janhuls <156079202+janhuls@users.noreply.github.com>
2024-01-16 11:24:11 +0100

    matrix class zonder numpy

--- matrix-class.py
+++ matrix-class.py
@@ -1,51 +1,41 @@
-import numpy as np
 class matrix:
     def __init__(self,matrix) -> None:
-        
         self.matrix = matrix
-        self.shape = matrix.shape
+        self.shape = [len(self.matrix),len(self.matrix[0])] #[rows,columns]
 
     def __str__(self):
         out = "[\n"
-        str_mat = np.char.mod("%d",self.matrix)
         for i in range(self.shape[0]):
             out+="["
             for j in range(self.shape[1]):
-                out+=str_mat[i][j]+"    "
+                out+=str(self.matrix[i][j])+"    "
             out = out[:-4]
             out+="]\n"
         out+="]"
         return out
-
+    
     def __add__(self,other):
         if self.shape!=other.shape:
             raise ValueError("matrices hebben niet dezelfde vorm")
-            pass
-        solmat = matrix(np.zeros(self.shape))
+        solmat = self
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                solmat.matrix[i][j]= self.matrix[i][j]+other.matrix[i][j]
-        for i in range(solmat.shape[0]):
-            for j in range(solmat.shape[1]):
-                solmat.matrix[i][j] = solmat.matrix[i][j]%2
+                solmat.matrix[i][j] = (other.matrix[i][j] + self.matrix[i][j])%2
         return solmat
+    
     def __radd__(self,other):
         if self.shape!=other.shape:
             raise ValueError("matrices hebben niet dezelfde vorm")
-            pass
-        solmat = matrix(np.zeros(self.shape))
+        solmat = self
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                solmat.matrix[i][j]= self.matrix[i][j]+other.matrix[i][j]
-        for i in range(solmat.shape[0]):
-            for j in range(solmat.shape[1]):
-                solmat.matrix[i][j] = solmat.matrix[i][j]%2
+                solmat.matrix[i][j] = (other.matrix[i][j] + self.matrix[i][j])%2
         return solmat
+    
     def __mul__(self,other):
         if self.shape[1]!=other.shape[0]:
             raise ValueError("matrices kunnen niet vermenigvuldigd worden!")
-            pass
-        solmat = matrix(np.zeros((self.shape[0],other.shape[1])))
+        solmat = matrix.nul_mat(self.shape[0],other.shape[1])
         for i in range(self.shape[0]):
             for j in range(other.shape[1]):
                 for k in range(other.shape[0]):
@@ -56,9 +46,17 @@ class matrix:
 
         return solmat
 
-y = np.array([[1,2,4],[3,4,1],[5,6,2]])
+    def nul_mat(row,col):
+        nul = []
+        for i in range(row):
+            nul.append([])
+            for j in range(col):
+                nul[i].append(0)
+        return matrix(nul)
+
+y = [[1,1,0],[1,1,1],[0,0,1]]
 A = matrix(y)
-x = np.array([[2,3,5],[1,3,2],[5,7,1]])
+x = [[0,1,0],[1,1,1],[0,0,0]]
 B = matrix(x)
 
-print(A+B)
+print(A*B)
